59671ecb04d9e79559dd865fd9d82e71715aab36
unknown <parmarsinghbhanu@gmail.com>
2023-02-22 22:26:28 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+import Aboutus from './Pages/Aboutus';
+import Contactus from './Pages/Contactus';
+import Home from './Pages/Home';
+import Ourteam from './Pages/Ourteam';
 
 function App() {
   return (
     
    <div>
-    
+    {/* <Footer/>
+    <Navbar/>
+    <Aboutus/>
+    <Contactus/> */}
+    <Ourteam/>
+    {/* <Home/> */}
    </div>
   );
 }
